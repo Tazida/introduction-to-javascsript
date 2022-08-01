64f1d28f92c43d28e96df41ebb5a7536f14fc155
Tazida <tazidarahman5@gmail.com>
2022-08-01 22:16:26 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,17 @@
+/***
+ * number ==> 55. 898, -2
+ * integer: 2, 5
+ * float: decimal: 2.5, 13.67, 32.69
+ * operation: addition, substraction, multiplication, division, remainder
+ * percent
+ * average
+ * comparison: >, <, ==, !=, >=, <=
+ * oikik: 2 kilo apple and 5 kilo mango
+ * 45, 25, 68, 129, 2: max, min, average, sum
+ * series
+ * :::EXTRA:::
+ * prime
+ * permutation
+ * :::ALWAYS:::
+ * loshagu , ghoshagu
+ */
---
+++ variable.js
@@ -0,0 +1,17 @@
+//vary + able = variable
+//numeric
+var price = 12;
+var age = 17;
+var temperature = 38;
+var weight = 50;
+var money = 1000;
+
+//sring
+var person = "tazida rahman";
+var location = "chayabithi bashaboo";
+var special = "leo messi";
+
+//boolean 0-false; 1-true;
+var serius = true;
+var isFullMark = true;
+var isSingle = false;
