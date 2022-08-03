ed0e78243627385cec94814469eac8ec6b0ad15a
Tazida <tazidarahman5@gmail.com>
2022-08-04 00:44:59 +0600

    full name

---
+++ conversion.js
@@ -0,0 +1,9 @@
+var price1 = 30;
+var price2 = 20;
+var totalPrice = price1 + price2;
+//console.log(totalPrice);
+
+var firstName = "jinne";
+var lastName = "kim";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
