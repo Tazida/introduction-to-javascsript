8f42bf71196a72695c1b67aef9f998bf9f51c390
Tazida <tazidarahman5@gmail.com>
2022-08-04 01:29:15 +0600

    added remainder

--- conversion.js
+++ conversion.js
@@ -6,4 +6,19 @@ var totalPrice = price1 + price2;
 var firstName = "jinne";
 var lastName = "kim";
 var fullName = firstName + " " + lastName;
-console.log(fullName);
+//console.log(fullName);
+
+var price1 = "30";
+var price2 = 10;
+//var totalPrice = price1 + price2;
+var totalPrice = price1 + " " + price2;
+console.log(totalPrice);
+
+//convert string to number
+console.log(price1);
+var price1Number = parseInt(price1);
+console.log(price1Number);
+
+var gpa = "3.41";
+var gpaNumber = parseFloat(gpa);
+console.log(gpaNumber);
---
+++ remainder.js
@@ -0,0 +1,6 @@
+var mangos = 19;
+var person = 7;
+var remainder = mangos % person;
+// / this means vagfol
+// % this means vagshesh
+console.log(remainder);
